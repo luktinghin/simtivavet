c32b707ae570a4070a6a3478cf9643e9c3e7fa0c
luktinghin <luktinghin@gmail.com>
2024-02-04 08:37:59 +0800

    .

--- main.js
+++ main.js
@@ -9176,6 +9176,11 @@ function readmodel(x, drug_set_index) {
 		"k21 = " + drug_sets[drug_set_index].k21 + "<br>" +
 		"k31 = " + drug_sets[drug_set_index].k31 + "<br>" + 
 		"ke0 = 0.723, from Bras (J Vet Pharmacol. Therap. 2008;32:182-188)";
+		if (state_premed == true) {
+			drug_sets[drug_set_index].modeltext += "<br>Assume presence of acepromazine-methadone premed)";
+		} else {
+			drug_sets[drug_set_index].modeltext += "<br>Assume absence of acepromazine-methadone premed)";
+		}
 
 		drug_sets[drug_set_index].drug_name = "Propofol";
 		drug_sets[drug_set_index].conc_units = "mcg";
